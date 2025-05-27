286dc62fabacbb22abd1962a60be5b2cd3792d4b
AbdelhamidOuazzou <ouazzouabdelhamid@gmail.com>
2025-05-27 23:07:14 +0000

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profile/', views.profile_view, name='profile'),
-    path('profile/edit/', views.profile_edit, name='profile_edit'),
-    path('register/', views.register, name='register'),
-] 
